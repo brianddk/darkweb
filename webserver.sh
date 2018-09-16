b17ae851328f729f99b9438f047bab3fbc19decd
brianddk <brianddk@users.noreply.github.com>
2018-09-16 16:49:15 +0000

    Added webserver.sh

---
+++ webserver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo noip2
+sudo swapon /mnt/2GB.swap
+cd ~/src/darkweb/
+sudo bundle exec jekyll serve -P 80 -H $HOSTNAME -d /var/lib/i2p/i2p-config/eepsite/docroot --detach
+vncserver
+
