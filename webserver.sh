a17e5ebb4e917312337b8a2cfd92dc5baa1e5c8b
brianddk <brianddk@users.noreply.github.com>
2018-09-17 13:51:20 +0000

    set ownership of web files

--- webserver.sh
+++ webserver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo noip2
 sudo swapon /mnt/2GB.swap
-(cd ~/src/darkweb/; sudo bundle exec jekyll serve -P 80 -H $HOSTNAME -d /var/lib/i2p/i2p-config/eepsite/docroot --detach)
+(cd ~/src/darkweb/; sudo bundle exec jekyll serve -P 80 -H $HOSTNAME -d /var/lib/i2p/i2p-config/eepsite/docroot --detach 1> /tmp/webserver.log 2>&1)
+(cd ~/src/darkweb/; sudo chown -R i2psvc:i2psvc /var/lib/i2p/i2p-config/eepsite/docroot)
 # ~/Freenet/run.sh start
 vncserver
